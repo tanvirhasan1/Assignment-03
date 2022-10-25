adf5b92fa739214422e4dce2fd8833941745b500
tanvirhasan1 <84311762+tanvirhasan1@users.noreply.github.com>
2022-10-25 23:36:53 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  
+    // mobile menu 
+    $('#mobile-menu').meanmenu({
+        meanScreenWidth: "767",
+        meanMenuContainer: '.mobile-menu'
+      });
+});
